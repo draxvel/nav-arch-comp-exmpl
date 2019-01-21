512900bd1abbeb15131b84aae953eefc0b3be3e9
draxvel <ntv3331998@gmail.com>
2019-01-22 01:59:17 +0200

    add: navigation on fragments.

--- app/src/main/java/com/tkachuk/navArchCompExmpl/MainFragment.kt
+++ app/src/main/java/com/tkachuk/navArchCompExmpl/MainFragment.kt
@@ -5,12 +5,24 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        goToProfileBtn.setOnClickListener (Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_profileFragment))
+
+        goToSettingsBtn.setOnClickListener { it.findNavController().navigate(R.id.action_mainFragment_to_settingsFragment) }
+
     }
 
 }
