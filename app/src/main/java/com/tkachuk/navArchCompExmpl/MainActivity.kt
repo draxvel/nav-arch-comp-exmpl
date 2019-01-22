629abbfb390e3e6b06df57c0ffd687ac29e9d228
nazar.tkachuk <nazar.tkachuk@vakoms.com.ua>
2019-01-22 16:20:07 +0200

    add: Navigation drawer

--- app/src/main/java/com/tkachuk/navArchCompExmpl/MainActivity.kt
+++ app/src/main/java/com/tkachuk/navArchCompExmpl/MainActivity.kt
@@ -6,12 +6,16 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NavigationUI.setupWithNavController(nav_view, findNavController(R.id.my_nav_host_fragment));
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
