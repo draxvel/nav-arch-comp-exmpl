b891bbaaf1b85c0e3c8b54bafaae69009f9afc84
draxvel <ntv3331998@gmail.com>
2019-01-22 02:21:58 +0200

    add: pass values from fragment to fragment

---
+++ app/src/main/java/com/tkachuk/navArchCompExmpl/NameFragment.kt
@@ -0,0 +1,23 @@
+package com.tkachuk.navArchCompExmpl
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_name.*
+
+class NameFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_name, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        nametv.text = arguments?.getString("nameArg")
+    }
+
+}
--- app/src/main/java/com/tkachuk/navArchCompExmpl/ProfileFragment.kt
+++ app/src/main/java/com/tkachuk/navArchCompExmpl/ProfileFragment.kt
@@ -5,12 +5,28 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.setOnClickListener {
+            val nameBundle = Bundle()
+            nameBundle.putString("nameArg", editText.text.toString())
+
+            it.findNavController().navigate(
+                    R.id.action_profileFragment_to_nameFragment,
+                    nameBundle)
+        }
+
     }
 
 }
